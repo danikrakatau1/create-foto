f714019b0b036c09bda459f274778453dd8f6ed8
danikrakatau1 <danikrakatau1@gmail.com>
2026-09-07 23:55:54 +0700

    chore: drive Remotion duration from scene config

--- remotion-japanese/src/Root.jsx
+++ remotion-japanese/src/Root.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import {Composition} from 'remotion';
 import {JapaneseScene} from './JapaneseScene';
+import {sceneConfig} from './scene-config';
 
 export const RemotionRoot = () => {
   return (
     <Composition
       id="JapaneseScene"
       component={JapaneseScene}
-      durationInFrames={240}
+      durationInFrames={sceneConfig.durationSeconds * 30}
       fps={30}
       width={1080}
       height={1920}
